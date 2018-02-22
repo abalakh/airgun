9b0a461e8ec9fdc9c607e92de996f1b8b2031129
Andrii Balakhtar <balakhtara@gmail.com>
2018-02-22 17:28:57 +0200

    Moved to https://github.com/SatelliteQE/airgun

--- airgun/views/common.py
+++
@@ -1,8 +0,0 @@
-from widgetastic.widget import View
-
-from ..widgets import HorizontalNavigation, ContextSelector
-
-
-class BaseLoggedInView(View):
-    menu = HorizontalNavigation()
-    taxonomies = ContextSelector()
